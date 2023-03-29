ca9ec6e171a2a6a990acf4f464e22bc7fbe341e2
Esteban S <esteban.samuelreyes@gmail.com>
2023-03-28 19:46:55 -0500

    using interfaces and redux

--- src/pages/deliver.tsx
+++ src/pages/deliver.tsx
@@ -3,19 +3,19 @@ import { props } from "cypress/types/bluebird";
 import { type } from "os";
 import * as React from "react";
 import { useState } from "react";
+import Theme from "@/models/themes";
+
+import { selectAuthState, setAuthState } from "../store/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Deliver() {
-    const options = Array.from(Array(6), (_, i) => ({ name: `Option ${i + 1}`, random: Array.from(Array(3), () => Math.round(Math.random() * 100)) }));
+    const options : Array<Theme> = Array.from(Array(6), (_, i) => ({ name: `Option ${i + 1}`, random: Array.from(Array(3), () => Math.round(Math.random() * 100)) }));
     const defaultProps = {
         options: options.map((option) => option.name),
         getOptionLabel: (option: string) => option,
         };
-    type UserData = {
-        name: string,
-        random: Array<number>
-      }
       // New State
-    type UserArray = Array<UserData>
+    type UserArray = Array<Theme>
     type userRandom = Array<number>
     type autoCompleteProps = { options: Array<string>, getOptionLabel: any };
     type autoCompleteArray = Array<autoCompleteProps>
@@ -23,6 +23,11 @@ export default function Deliver() {
     const [users, setUsers] = useState<UserArray>(options)
     const [user , setUser] = useState<userRandom | null>(null)
     const [secondOption , setOption] = useState<userRandom | null>(null) 
+    const authState = useSelector(selectAuthState);
+    const dispatch = useDispatch();
+    console.log(authState);
+    dispatch(setAuthState(false))
+    console.log(authState);
     function getRandom(value: string){
         let random: userRandom | undefined ;
         random  = options.find((option) => option.name === value)?.random;
